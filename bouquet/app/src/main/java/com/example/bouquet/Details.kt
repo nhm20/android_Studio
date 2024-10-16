ed39e029dc3f6a284821fb5f58f93a44ebebd21a
nhm20 <nmo212182@gmail.com>
2024-10-16 10:13:21 +0530

    Bouquet App

---
+++ bouquet/app/src/main/java/com/example/bouquet/Details.kt
@@ -0,0 +1,4 @@
+package com.example.bouquet
+
+class Details {
+}
---
+++ bouquet/app/src/main/java/com/example/bouquet/HomePage.kt
@@ -0,0 +1,29 @@
+package com.example.bouquet
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class HomePage : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        var btn = findViewById<Button>(R.id.sbm)
+        var name = findViewById<EditText>(R.id.name)
+        var pass = findViewById<EditText>(R.id.pass)
+        btn.setOnClickListener {
+            var a = name.text.toString()
+            var b = pass.text.toString()
+            val i = Intent(this, HomePage::class.java)
+            i.putExtra("name", a)
+            i.putExtra("pass", b)
+            startActivity(i)
+        }
+    }
+}
